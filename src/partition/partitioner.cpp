589ace603891b34a957cae0329a837fb803a01b9
Patrick Niklaus <patrick.niklaus@student.kit.edu>
2017-03-15 18:16:54 +0000

    Refactor unconnected node removal in own function

--- src/partition/partitioner.cpp
+++ src/partition/partitioner.cpp
@@ -1,5 +1,6 @@
 #include "partition/partitioner.hpp"
 #include "partition/bisection_graph.hpp"
+#include "partition/remove_unconnected.hpp"
 #include "partition/bisection_to_partition.hpp"
 #include "partition/cell_storage.hpp"
 #include "partition/compressed_node_based_graph_reader.hpp"
@@ -208,6 +209,7 @@ int Partitioner::Run(const PartitionConfig &config)
                      edge_based_partition_ids[backward_node] != SPECIAL_NODEID);
     }
 
+
     std::vector<Partition> partitions;
     std::vector<std::uint32_t> level_to_num_cells;
     std::tie(partitions, level_to_num_cells) =
@@ -217,74 +219,8 @@ int Partitioner::Run(const PartitionConfig &config)
                               config.minimum_cell_size * 32 * 16,
                               config.minimum_cell_size * 32 * 16 * 32});
 
-    auto num_unconnected = 0;
-    for (int level_index = partitions.size() - 1; level_index >= 0; level_index--)
-    {
-        struct Witness
-        {
-            NodeID id;
-            std::size_t induced_border_edges;
-        };
-        std::vector<Witness> witnesses;
-        for (NodeID node = 0; node < edge_based_graph->GetNumberOfNodes(); ++node)
-        {
-            witnesses.clear();
 
-            bool is_source = false;
-            bool is_target = false;
-
-            const auto cell_id = partitions[level_index][node];
-            for (auto edge : edge_based_graph->GetAdjacentEdgeRange(node))
-            {
-                const auto data = edge_based_graph->GetEdgeData(edge);
-                const auto target = edge_based_graph->GetTarget(edge);
-                const auto target_cell_id = partitions[level_index][target];
-                if (target_cell_id == cell_id)
-                {
-                    is_source |= data.forward;
-                    is_target |= data.backward;
-                }
-                else
-                {
-                    witnesses.push_back({target, 0});
-                }
-            }
-
-            const auto unconnected = witnesses.size() > 0 && !is_source && !is_target;
-
-            if (unconnected)
-            {
-                num_unconnected++;
-                for (auto &witness : witnesses)
-                {
-                    for (auto edge : edge_based_graph->GetAdjacentEdgeRange(node))
-                    {
-                        auto target = edge_based_graph->GetTarget(edge);
-                        for (auto sublevel_index = level_index; sublevel_index >= 0;
-                             --sublevel_index)
-                        {
-                            if (partitions[sublevel_index][target] !=
-                                partitions[sublevel_index][witness.id])
-                                witness.induced_border_edges++;
-                        }
-                    }
-                }
-
-                auto best_witness = std::min_element(
-                    witnesses.begin(), witnesses.end(), [](const auto &lhs, const auto &rhs) {
-                        return lhs.induced_border_edges < rhs.induced_border_edges;
-                    });
-                BOOST_ASSERT(best_witness != witnesses.end());
-
-                // assign `node` to same subcells as `best_witness`
-                for (auto sublevel_index = level_index; sublevel_index >= 0; --sublevel_index)
-                {
-                    partitions[sublevel_index][node] = partitions[sublevel_index][best_witness->id];
-                }
-            }
-        }
-    }
-
+    auto num_unconnected = removeUnconnectedBoundaryNodes(*edge_based_graph, partitions);
     util::Log() << "Fixed " << num_unconnected << " unconnected nodes";
 
     util::Log() << "Edge-based-graph annotation:";
